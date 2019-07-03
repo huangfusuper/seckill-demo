bf656bea27a99f58eef5d50b64e18bf3dd964f71
huangfu <huangpukexing>
2019-07-03 13:30:05 +0800

    更改部分注释

--- src/main/java/com/demo/ClientThread.java
+++ src/main/java/com/demo/ClientThread.java
@@ -35,15 +35,27 @@ public class ClientThread implements Runnable{
             System.out.println("顾客:" + clientName + "开始抢商品");
             jedis = RedisUtil.getInstance().getJedis();
             try {
-                //一般是和事务一起使用，当对某个key进行watch后如果其他的客户端对这个key进行了更改，那么本次事务会被取消，事务的exec会返回null。jedis.watch(key)都会返回
+                /**
+                 * 一般是和事务一起使用，当对某个key进行watch后如果其他的客户端对这个key进行了更改，
+                 * 那么本次事务会被取消，事务的exec会返回null。jedis.watch(key)都会返回OK
+                 */
                 jedis.watch(key);
                 //当前商品的个数
                 int prdNum = Integer.parseInt(jedis.get(key));
                 if(prdNum>0){
-                    //创建Redis事务
+                    /**
+                     *  创建Redis事务
+                     *  事务是保证事务内的所有命令是原子操作，一般配合watch使用，
+                     *  事务的执行结果和pipeline一样都是采用异步的方式获取结果，
+                     *  multi.exec()提交事务，
+                     *  如果执行成功，其返回的结果和pipeline一样是所有命令的返回值，
+                     *  如果事务里面有两个命令那么事务的exec返回值会把两个命令的返回值组合在一起返回。
+                     *  如果事务被取消返回null。
+                     */
                     Transaction multi = jedis.multi();
                     multi.set(key,String.valueOf(prdNum-1));
-                    //如果客户端在使用 MULTI 开启了一个事务之后，却因为断线而没有成功执行 EXEC ，那么事务中的所有命令都不会被执行。
+                    //如果客户端在使用 MULTI 开启了一个事务之后，
+                    // 却因为断线而没有成功执行 EXEC ，那么事务中的所有命令都不会被执行。
                     List<Object> exec = multi.exec();
                     //没抢到
                     if(exec==null || exec.isEmpty()){
